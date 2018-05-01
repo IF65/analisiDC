3340099ee43460720e2758894a1720fbc5e294ad
Marco Gnecchi <marco.gnecchi@if65.it>
2018-05-01 04:22:55 +0200

    riorganizzazione cartelle

--- examples/analisiDC.php
+++ examples/analisiDC.php
@@ -1,9 +1,12 @@
 <?php
+    require '../vendor/autoload.php';
+    
     include_once("../Dc/Dc.php");
    
     use Dc\Dc  ;
 
     $test = new Dc("../examples/3671_20171210_171210_DC.TXT");
     $test->mostraInformazioni();
-
+    
+    
 ?>
