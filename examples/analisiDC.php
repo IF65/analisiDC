4823f65585bfca02fe294c46c0b49f14a6b8ba08
Marco Gnecchi <marco.gnecchi@icloud.com>
2018-05-03 10:48:28 +0200

    Aggiunto supporto x Anagdafi

--- examples/analisiDC.php
+++ examples/analisiDC.php
@@ -1,12 +1,18 @@
 <?php
     require '../vendor/autoload.php';
+    include('../src/Database/bootstrap.php');
     
     require_once '../src/Dc/Dc.php';
     
     use Dc\Dc;
+    use Database\Tables\Anagdafi;
 
     $test = new Dc("../examples/data/3671_20171210_171210_DC.TXT");
     $test->mostraInformazioni();
+
+	$anagdafi = new Anagdafi($sqlDetails);
     
-    
+    $prezzi = $anagdafi->prezziDelGiorno('2018-05-02', '0101');
+    print_r($prezzi);
+	
 ?>
