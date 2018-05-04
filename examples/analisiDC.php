1576acd2a64336d51237253530b6a0e7906dfa60
Marco Gnecchi <marco.gnecchi@if65.it>
2018-05-04 07:24:15 +0200

    varie

--- examples/analisiDC.php
+++ examples/analisiDC.php
@@ -12,6 +12,20 @@
 
 	$anagdafi = new Database($sqlDetails);
     
-    //$prezziLocali = $anagdafi->prezziDelGiorno('2018-05-02', '0102');
-	
+    $elencoPrezziLocali = $anagdafi->prezziDelGiorno('2018-05-02', '0102');
+    
+    $prezziLocali =[];
+    foreach ($elencoPrezziLocali['data'] as $prezzoLocale) {
+        $prezziLocali[$prezzoLocale['codice']] = $prezzoLocale;
+    }
+    print_r($prezziLocali);
+    
+	/*
+select a.`COD-ART2` `codice`, a.`DES-ART2` `descrizione`, ifnull(d.`REPARTO_CASSE`,1) `reparto`, case when a.`COD-IVA-ART2` = 2100 then 2200 else a.`COD-IVA-ART2` end `codiceIva` ,a.`IVA-ART2` `aliquotaIva` from archivi.articox2 as a left join dimensioni.articolo as d on a.`COD-ART2`=d.`CODICE_ARTICOLO` order by 1;
+
+select b.`CODCIN-BAR2` `codice`, b.`BAR13-BAR2` `barcode` from archivi.barartx2 as b order by 1; 
+
+select b.`BAR13-BAR2`, count(*) from archivi.barartx2 as b group by 1 having count(*) > 1; 
+*/
 ?>
+
