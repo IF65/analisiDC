efa03bbe1db2fc2b7a5ee0c81ccfb43acbaf9add
Marco Gnecchi <marco.gnecchi@if65.it>
2018-05-11 08:46:30 +0200

    closure

--- src/Datacollect/Transazioni/Transazione.php
+++ src/Datacollect/Transazioni/Transazione.php
@@ -40,6 +40,7 @@
                 $this->numero = $matches[10];
             }
 
+            $righeBeneficio = [];
             foreach ($this->righe as $riga) {
                 // numero di righe del datacollect
                 if (preg_match('/^.{31}:.:1/', $riga)) {
@@ -64,6 +65,11 @@
                 if (preg_match('/^.{31}:S:1/', $riga)) {
                     $this->vendite[] = new Vendita($riga, $this->db);
                 }
+                
+                // carico le righe che fanno parte di un beneficio per eseguire successivamente l'analisi e il loro caricamento
+                if (preg_match('/^.{31}:(C|D|G|d|m|w):1/', $riga)) {
+                    $righeBeneficio[] = $riga;
+                }                
 
                 // carico le forme di pagamento
                 if (preg_match('/^.*:T:1.{25}(\d\d).{6}((?:\+|\-)\d{9})$/', $riga, $matches)) {
@@ -74,6 +80,33 @@
                     }
                 }
             }
+            
+            // carico i benefici
+            $esito = function() use(&$righeBeneficio) {
+                for ( $i = 0 ; $i < count($righeBeneficio); $i++) {
+                    $numeroRigheBlocco = 0;
+                    
+                    if ((($i + 2) < count($righeBeneficio)) and preg_match('/:C:142:\d{4}:P0:(.{13})((?:\-|\+)\d{4}).{4}(\*|\+|\-)(\d{9})$/', $righeBeneficio[$i], $matches)) {
+                        $barcodeC = $matches[1];
+                        $quantitaC = $matches[2];
+                        $operazioneC = $matches[3];
+                        $importoC = $matches[4];
+                        if (preg_match('/:G:131:\d{4}:P0:(.{13}):..((?:\-|\+)\d{5})(\*|\+|\-)(\d{9})$/', $righeBeneficio[$i + 1], $matches)) {
+                            $barcodeG = $matches[1];
+                            $puntiG = $matches[2];
+                            $operazioneG = $matches[3];
+                            $importoG = $matches[4];
+                            if (preg_match('/:m:1.{7}:0027/', $righeBeneficio[$i + 2])) {
+                                $righeBeneficio = array_slice($righeBeneficio,$i + 3);
+                                return true;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            };
+            
+            while ($esito) {}
         }
 
         function __destruct() {}
