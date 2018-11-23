7fd2041d2987d66fe1ed3aa5f10970fea05140c3
Marco Gnecchi <marco.gnecchi@icloud.com>
2018-11-23 16:01:23 +0100

    allineamento

---
+++ examples/creaFattura.php
@@ -0,0 +1,21 @@
+<?php
+    @ini_set('memory_limit','512M');
+
+    require(realpath(__DIR__ . '/..').'/vendor/autoload.php');
+    require(realpath(__DIR__ . '/..').'/src/Database/autoload.php');
+    require(realpath(__DIR__ . '/..').'/src/Datacollect/autoload.php');
+
+    use Database\Database;
+
+    $timeZone = new \DateTimeZone('Europe/Rome');
+    
+    echo '- Inizio elaborazione            : '.(new \DateTime())->setTimezone($timeZone)->format('H:i:s')."\n\n";
+    $db = new Database($sqlDetails);
+    
+    $output = [];
+	
+	$result = exec ('perl /script/mtxGetTransactionList.pl -d 2018-11-23 -h 0110', $output );
+	
+	print_r($output);
+?>
+
