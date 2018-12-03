342c3c43ff4e7f25bf9d49af4474150df3b70090
Marco Gnecchi <marco.gnecchi@icloud.com>
2018-12-03 10:47:33 +0100

    allineamento

--- examples/creaFattura.php
+++ examples/creaFattura.php
@@ -17,12 +17,14 @@
     
     $input = file_get_contents('php://input');
     $request = json_decode($input, true);
-    
+    echo $input;
    	if ( ! isset( $request ) ) {
         die;
     }
     
     if ($request['function'] == 'creaFattura') {
+        echo creaFattura($request);
+    } else if ($request['function'] == 'elencoTransazioni') {
         echo creaFattura($request);
     }
     
@@ -103,6 +105,32 @@
         $fattura['righe'] = $righeFattura;
         
         return json_encode($fattura);
+    }
+    
+    
+    function elencoTransazioni(array $request) {
+        // variabili
+        global $sqlDetails;
+    
+        // costanti
+        $ivaAliquota = [ 0 => 0, 1 => 4, 2 => 10, 3 => 22, 4 => 0, 5 => 0, 6 => 0, 7 => 0 ];
+        $ivaDescrizione = [ 0 => 'IVA 0', 1 => 'IVA 4', 2 => 'IVA 10', 3 => 'IVA 22', 4 => 'IVA 5', 5 => 'ES.GIFT.92', 6 => 'ES.GIFT 93/94', 7 => 'ES. VARI' ];
+        
+        // carico il database
+        $db = new Database($sqlDetails);
+        
+        // recupero il dc
+        $client = new GuzzleHttp\Client();
+        
+        $url = 'http://10.11.14.77/eFatture/eFatture.php';
+        $headers = array('Content-Type: application/json');
+        
+        $postRequest = new GuzzleHttp\Psr7\Request("POST", $url, $headers, json_encode(['function' => 'getTransaction', $request]));
+        
+        $response = $client->send($postRequest, ['timeout' => 20]);
+        $responseJson = $response->getBody()->getContents();     
+        
+        return $responseJson;
     }
 ?>
 
