3e82ac224074e189e4904ae8c58f2dde4a96a672
Marco Gnecchi <marco.gnecchi@icloud.com>
2019-04-08 21:02:34 +0200

    Aggiunta documentazione

--- examples/analisiDC.php
+++ examples/analisiDC.php
@@ -1,27 +1,27 @@
 <?php
-    @ini_set('memory_limit','512M');
+@ini_set( 'memory_limit', '512M' );
 
-    require(realpath(__DIR__ . '/..').'/vendor/autoload.php');
-    require(realpath(__DIR__ . '/..').'/src/Database/autoload.php');
-    require(realpath(__DIR__ . '/..').'/src/Datacollect/autoload.php');
+require(realpath( __DIR__ . '/..' ) . '/vendor/autoload.php');
+require(realpath( __DIR__ . '/..' ) . '/src/Database/autoload.php');
+require(realpath( __DIR__ . '/..' ) . '/src/Datacollect/autoload.php');
 
-    use Datacollect\Datacollect;
-    use Database\Database;
+use Datacollect\Datacollect;
+use Database\Database;
 
-    $timeZone = new \DateTimeZone('Europe/Rome');
-    
-    echo '- Inizio elaborazione            : '.(new \DateTime())->setTimezone($timeZone)->format('H:i:s')."\n\n";
-    $db = new Database($sqlDetails);
+$timeZone = new DateTimeZone( 'Europe/Rome' );
 
-    echo '- Caricamento prezzi locali      : '.(new \DateTime())->setTimezone($timeZone)->format('H:i:s')."\n";
-    //$prezziLocali = $db->ricercaPrezziLocali(['codiceNegozio' => '0133', 'data' => '2018-05-17']);
-    
-    echo '- Caricamento datacollect        : '.(new \DateTime())->setTimezone($timeZone)->format('H:i:s')."\n\n";
-    $test = new Datacollect(realpath(__DIR__ . '/..')."/examples/data/0133_20180517_180517_DC.TXT", $db);
-    
-    echo '- Stampa transazioni             : '.(new \DateTime())->setTimezone($timeZone)->format('H:i:s')."\n\n";
-    $test->stampaTransazioni();
-    
-    echo '- Fine elaborazione              : '.(new \DateTime())->setTimezone($timeZone)->format('H:i:s')."\n\n";
+echo '- Inizio elaborazione            : ' . (new DateTime())->setTimezone( $timeZone )->format( 'H:i:s' ) . "\n\n";
+$db = new Database( $sqlDetails );
+
+echo '- Caricamento prezzi locali      : ' . (new DateTime())->setTimezone( $timeZone )->format( 'H:i:s' ) . "\n";
+//$prezziLocali = $db->ricercaPrezziLocali(['codiceNegozio' => '0133', 'data' => '2018-05-17']);
+
+echo '- Caricamento datacollect        : ' . (new DateTime())->setTimezone( $timeZone )->format( 'H:i:s' ) . "\n\n";
+$test = new Datacollect( realpath( __DIR__ . '/..' ) . "/examples/data/0133_20180517_180517_DC.TXT", $db );
+
+echo '- Stampa transazioni             : ' . (new DateTime())->setTimezone( $timeZone )->format( 'H:i:s' ) . "\n\n";
+$test->stampaTransazioni();
+
+echo '- Fine elaborazione              : ' . (new DateTime())->setTimezone( $timeZone )->format( 'H:i:s' ) . "\n\n";
 ?>
 
